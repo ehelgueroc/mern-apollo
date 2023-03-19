66d70ff92f6911a7ba3a1ebb545780f1e355f792
Esteban Helguero <esteban.helguero@s4rb.com>
2023-03-19 08:39:28 +0000

    Created tasks resolvers and definitions

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,10 +1,14 @@
 import Project from "../models/Project.js";
+import Task from "../models/Task.js";
 
 export const resolvers = {
     Query: {
         hello: () => "Hello",
         projects: async () => {
             return await Project.find()
+        },
+        tasks: async () => {
+            return await Task.find()
         }
     },
     Mutation: {
@@ -15,6 +19,17 @@ export const resolvers = {
             });
             const savedProject = await project.save();
             return savedProject;
+        },
+        createTask: async (_, {title, projectId}) => {
+            const project = await Project.findById(projectId);
+            if(!project) throw new Error("Project not found");
+
+            const task = new Task({
+                title,
+                projectId
+            });
+            const savedTask = task.save();
+            return savedTask;
         }
     }
 }
